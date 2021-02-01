6c25e4a11fd1144d2cd4c5103faea058dac377d1
Alex <alex@freedcamp.com>
2021-02-01 19:13:03 +0200

    [CONFIG] Locations and validator configs added

---
+++ html/application/config/form_validation.php
@@ -0,0 +1,57 @@
+<?php
+$config = array(
+    'customer' => array(
+        array(
+            'field' => 'gender',
+            'label' => 'Gender',
+            'rules' => 'required|in_list[male,female,not_to_say]'
+        ),
+        array(
+            'field' => 'first_name',
+            'label' => 'First name',
+            'rules' => 'required|max_length[50]'
+        ),
+        array(
+            'field' => 'last_name',
+            'label' => 'Last name',
+            'rules' => 'required|max_length[50]'
+        ),
+        array(
+            'field' => 'country_code',
+            'label' => 'Country',
+            'rules' => 'required|exact_length[2]|valid_country'
+        ),
+        array(
+            'field' => 'email',
+            'label' => 'Email',
+            'rules' => 'required|valid_email|email_is_unique'
+        ),
+    ),
+    'edit_customer' => array(
+        array(
+            'field' => 'gender',
+            'label' => 'Gender',
+            'rules' => 'in_list[male,female,not_to_say]'
+        ),
+        array(
+            'field' => 'first_name',
+            'label' => 'First name',
+            'rules' => 'max_length[50]'
+        ),
+        array(
+            'field' => 'last_name',
+            'label' => 'Last name',
+            'rules' => 'max_length[50]'
+        ),
+        array(
+            'field' => 'country_code',
+            'label' => 'Country',
+            'rules' => 'exact_length[2]|valid_country'
+        ),
+        array(
+            'field' => 'email',
+            'label' => 'Email',
+            'rules' => 'valid_email'
+        )
+    )
+);
